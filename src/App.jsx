a1dfdc8dc7546570701457b88e040db0f7277a48
thanmai2903 <thanmaipalla329@gmail.com>
2025-03-21 12:24:11 +0530

    adding website

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/header.jsx";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,130 @@
+export function Header() {
+  return (
+    <div>
+      <div className="div1">LORD SHIVA</div>
+      <div className="div2">Who is Lord Shiva?</div>
+      <img
+        className="img1"
+        src="https://www.dadabhagwan.org/images/trimandir/shiva/Lord-Shiva.jpg"
+      />
+      <div className="para">
+        According to scriptures, Lord Shiva is God who was residing in the
+        Himalayan Mountains. Parvati was his wife and Ganesh and Kartik were His
+        sons. These are physical attributes but in true sense any human being
+        who attains 'Self Realization' achieves the internal state of 'Shiva'.
+      </div>
+      <div className="head">Shiva Symbolizes the Embodiment of Salvation</div>
+      <img
+        className="img1"
+        src="https://www.dadabhagwan.org/images/trimandir/shiva/Lord-Shiva-2.jpg"
+      />
+      <div className="para">
+        In true meaning, the One who has become ‘kalyan swaroop’ (the embodiment
+        of salvation) is called Shiva. Thus, Shiva symbolizes the embodiment of
+        salvation i.e. the Gnani.
+      </div>
+      <div className="head">Lord Shiva is also known as Neelkanth</div>
+      <div className="para">
+        The story suggests that, when Lord Shiva drank the venom that came out
+        during the Samudra-Manthan, due to the poison that he consumed, his
+        throat became blue and hence he got the name Neelkanth. The essence
+        behind the word "Neelkanth" is that one who has swallowed all the
+        poisons (abuses and insults) given to Him in this world with equanimity
+        and yet offered blessings to others, hence becoming Lord Shiva.
+      </div>
+      <img
+        className="img1"
+        src="https://www.dadabhagwan.org/images/trimandir/shiva/Lord-Shiva-3.jpg"
+      />
+      <div className="para">
+        It means that if someone says something that we do not like, then we
+        should drink the insult peacefully. Moreover, we shall pray for the
+        person accusing us, “May God give him the right understanding.” When we
+        do this, we too will become Lord Shiva one day. All Enlightened beings
+        have done this in their life. That’s why they are called ‘kalyan
+        swaroop’
+      </div>
+      <h1 className="h">Param Pujya Dada Bhagwan says...</h1>
+      <div className="para">
+        “You will certainly have to drink the poison. It is your karmic account,
+        so the glass [of poison] will certainly come before you. Then whether
+        you choose to drink it with a smile or whether you drink it with a
+        sullen face, you will have to drink it. Hey! Even if you do not want to
+        drink it at all, people will make you drink it forcibly. Instead, why
+        not drink it with a smile, while giving the person blessings? Besides
+        this, how else can you become Neelkantha?
+      </div>
+      <div className="para">
+        Those who come and give you these glasses are actually coming to raise
+        You to an elevated state. However, if you have a sullen face at that
+        point, then that [state] will move away from you. Whoever in this world
+        has given me glasses of poison, I have drunk them with a smiling face,
+        while blessing them, and have become Mahadev(Lord Shiva).
+      </div>
+      <div className="para">
+        If the other person gives you something bitter to drink, and you drink
+        it with a smiling face while giving blessings, then on one hand your ego
+        will be cleansed and you will become free to that extent. And on the
+        other hand, the other person, the one who gives the bitter drink, will
+        also turn back. He will also feel better. He will realize, ‘It is my
+        weakness to serve him this bitter drink, and this person who is drinking
+        it with a smile on his face, is extremely powerful.’
+      </div>
+      <iframe
+        className="img1"
+        width="1000"
+        height="500"
+        src="https://www.youtube.com/embed/4wfuxf-2av8?si=B9y1GrOWdp2NEmdg"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+      <div className="head">
+        The Ultimate transition from Om Namah Shivay to Shivoham Shivoham0
+      </div>
+      <div className="para">
+        Lord Shiva shown in the statue is a symbol for worshipping the Soul.
+        Therefore, we say “Om Namo Shivay!”
+      </div>
+      <div className="para">
+        Gnani says, “This is the real stage. If you want to achieve the
+        Shiv-dasha, you have to realize the Self i.e. who am I.” However, we
+        believe the statue to be God and we pray. By praying, we are developing
+        the relative part (that which we consider to be our self, other than the
+        Soul), as a result of which we will get worldly happiness i.e. a good
+        wife, good children, good religion, etc.
+      </div>
+      <div className="para">
+        But when we realize our own Soul, automatically our path of salvation
+        starts. And then, we will get a higher and higher relative level on the
+        outside, and inside we will be getting liberation (mukti).
+      </div>
+      <img
+        className="img1"
+        src="https://www.dadabhagwan.org/images/trimandir/shiva/Lord-Shiva-4.jpg"
+      />
+      <div className="para">
+        So, in the ignorant state (Jiv dasha), since one considers himself
+        separate from Shiva, one says 'Om Namah Shivay' (I bow to Shiva). But
+        after attaining Self Realization, one attains the constant awareness of
+        Soul, separate from the body, and then he experiences ‘Shivoham,
+        Shivoham... (I am Shiva, I am Shiva). I am a Soul and not the body’.
+        Thereafter, there remains no distinction between Lord Shiva and one's
+        true self i.e. the Soul.
+      </div>
+      <div className="para">
+        Param Pujya Dadashri achieved Self Realization (experience of the Soul)
+        in June 1958. He used to impart the same 'Self Realization' to others in
+        just 2 hours. Pujya Dr. Niruma attained Self Realization from Param
+        Pujya Dadashri in 1968.
+      </div>
+      <div className="para">
+        You too can experience Self Realization by attending 'Gnan Vidhi' by
+        Pujya Deepakbhai. After receiving Gnan and by observing five Agnas (the
+        fundamental principles) you will be led to ultimate salvation.
+      </div>
+    </div>
+  );
+}
